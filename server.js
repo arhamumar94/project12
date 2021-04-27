3b28b78e7359a7b7095ea86da6cd0bdc36e24916
arhamumar94 <arhamumar1999@gmail.com>
2021-04-28 01:07:36 +0530

    hey

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express =require('express')
 const path =require('path')
-const {db} =require('./db/models')
+const {db} =require('./db')
 const app=express();
 const PORT=process.env.PORT||2678;
 app.use(express.json())
